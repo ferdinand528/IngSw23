37fe46c02959e8ac0a3b4190b926d1336538c7b0
Fernando Alvarez Hayes <fernandoalvarez1990@gmail.com>
2023-06-11 21:23:36 -0300

    probamos nuevo deploy MAXI

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -10,6 +10,7 @@ var port = 3001;
 app.get('/', function (req, res) {
   var resultado = sumar(2, 5);
   res.send("El resultado de la suma es: ".concat(resultado));
+  console.log('Esto funciona parece');
 });
 app.listen(port, function () {
   console.log('Servidor Express en funcionamiento en el puerto 3001');
